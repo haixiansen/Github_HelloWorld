3212258efdf3e38232b935219c96507b2dc595a3
moliying <576146108@qq.com>
2016-08-09 22:02:15 +0800

    first submit

--- app/src/main/java/learning/moliying/com/github_helloworld/MainActivity.java
+++ app/src/main/java/learning/moliying/com/github_helloworld/MainActivity.java
@@ -1,8 +1,14 @@
 package learning.moliying.com.github_helloworld;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+        System.out.println("branch");
+    }
 }
